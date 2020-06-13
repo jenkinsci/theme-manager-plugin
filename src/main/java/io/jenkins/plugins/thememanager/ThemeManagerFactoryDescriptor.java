194bc3157f456144812da37b1a12561f50fc4733
Tim Jacomb <timjacomb1+github@gmail.com>
2020-06-13 09:20:34 +0100

    Add JavaDoc

--- src/main/java/io/jenkins/plugins/thememanager/ThemeManagerFactoryDescriptor.java
+++ src/main/java/io/jenkins/plugins/thememanager/ThemeManagerFactoryDescriptor.java
@@ -4,18 +4,61 @@ import hudson.DescriptorExtensionList;
 import hudson.model.Descriptor;
 import jenkins.model.Jenkins;
 
+/**
+ * Definition of a Theme Manager
+ * @see ThemeManagerFactory
+ */
 public abstract class ThemeManagerFactoryDescriptor extends Descriptor<ThemeManagerFactory> {
-
+  
+  /**
+   * Creates an instance of a {@link ThemeManagerFactory}.
+   * 
+   * @return an instance of {@link ThemeManagerFactory}
+   */
   public abstract ThemeManagerFactory getInstance();
 
+  /**
+   * A unique name for a theme plugin
+   * 
+   * This should be all lower case and URL safe,
+   * i.e. 'dark', 'neo2'
+   * 
+   * The ID can be re-used inside of your plugin if you are serving multiple variations
+   * of the theme, i.e. 'Dark', 'Dark (System)' could both be 'dark'.
+   * 
+   * Used in generating URLs for Jenkins to find your theme files.
+   * 
+   * @return the theme ID.
+   */
+  public abstract String getThemeId();
+
+  /**
+   * Name of the theme resource file.
+   * 
+   * Used in generating URLs for Jenkins to find your theme files.
+   * 
+   * @return name of the theme resource file.
+   */
   public String getThemeCssSuffix() {
     return "theme.css";
-  };
+  }
 
+  /**
+   * Name of the theme resource file.
+   *
+   * Used in generating URLs for Jenkins to find your theme files.
+   *
+   * @return name of the theme resource file.
+   */
   public String getThemeJsSuffix() {
     return "theme.js";
-  };
+  }
 
+  /**
+   * All implementations of {@link ThemeManagerFactory} and {@link ThemeManagerFactoryDescriptor}.
+   * 
+   * @return descriptor extension list containing all themes.
+   */ 
   public static DescriptorExtensionList<ThemeManagerFactory, ThemeManagerFactoryDescriptor> all() {
     return Jenkins.get().getDescriptorList(ThemeManagerFactory.class);
   }
--- src/main/java/io/jenkins/plugins/thememanager/ThemeManagerSimplePageDecorator.java
+++ src/main/java/io/jenkins/plugins/thememanager/ThemeManagerSimplePageDecorator.java
@@ -2,8 +2,11 @@ package io.jenkins.plugins.thememanager;
 
 import hudson.Extension;
 import jenkins.model.SimplePageDecorator;
+import org.kohsuke.accmod.Restricted;
+import org.kohsuke.accmod.restrictions.NoExternalUse;
 
 @Extension
+@Restricted(NoExternalUse.class)
 public class ThemeManagerSimplePageDecorator extends SimplePageDecorator {
 
   public ThemeManagerSimplePageDecorator() {
