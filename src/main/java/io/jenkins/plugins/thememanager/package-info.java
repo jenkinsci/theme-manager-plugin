2a0fa0455fa7641606e47cd807c292ef7db97bdf
Tim Jacomb <timjacomb1@gmail.com>
2022-12-01 20:21:03 +0000

    Try a workaround

--- src/main/java/io/jenkins/plugins/thememanager/package-info.java
+++ src/main/java/io/jenkins/plugins/thememanager/package-info.java
@@ -1,3 +1,4 @@
+/** Dummy javadoc comment till 11.0.18 is released https://bugs.openjdk.org/browse/JDK-8297889 */
 @DefaultAnnotationForParameters(NonNull.class)
 package io.jenkins.plugins.thememanager;
 
